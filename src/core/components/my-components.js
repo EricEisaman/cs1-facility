1f0041d268cefa3a9a5b8b850eacd75b1e3d24a5
Glitch (shaderfac) <none>
2019-12-12 22:07:28 +0000

    v0.7.16  custom aframe-v0.9.2

--- src/core/components/my-components.js
+++ src/core/components/my-components.js
@@ -1,7 +1,28 @@
 export default(()=>{
   
+function thruster(offset) {
+    const t = document.createElement("a-sphere");
+    t.setAttribute("radius", "0.4");
+    t.setAttribute("position", offset);
+    t.setAttribute("shader-frog", "name:Thruster_Shader");
+    return t;
+  }
+  
 document.addEventListener('gameStart',e=>{
-    
+  
+  CS1.shaderfrog.Thruster_Shader.uniforms.backgroundColor.value.r = "0.4";
+  CS1.shaderfrog.Thruster_Shader.uniforms.backgroundColor.value.g = "0.1";
+  CS1.shaderfrog.Thruster_Shader.uniforms.backgroundColor.value.b = "0.2";
+  
+  CS1.scene.appendChild(thruster("-20.75 2.52 -21.4"));
+  CS1.scene.appendChild(thruster("-16.9 2.52 -21.4"));
+  CS1.scene.appendChild(thruster("-9.21 2.52 -21.4"));
+  CS1.scene.appendChild(thruster("-5.54 2.52 -21.4"));
+  
+  CS1.scene.appendChild(thruster("-20.75 2.52 -14.36"));
+  CS1.scene.appendChild(thruster("-16.9 2.52 -14.36"));
+  CS1.scene.appendChild(thruster("-9.21 2.52 -14.36"));
+  CS1.scene.appendChild(thruster("-5.54 2.52 -14.36"));
 
      
 });  
